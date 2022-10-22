80c095cce5881e8db16f1228ef3c8e5110e00b89
Nikkeljohn <nikkeljohn120@gmail.com>
2022-10-22 00:35:50 +0000

    used arrays for tile so it can wrap all objects for the tile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
+// added an event listener to get game button element and content
+
 window.addEventListener('DOMContentLoaded', () =>{
+    const announcer = document.querySelector('.announcer');
+    const playerDisplay = document.querySelector('.display-player');
+    const tile = Array.from(document.querySelectorAll('.tile'));
+    const resetButton = document.querySelector('#reset');
 
     resetButton.addEventListener('click', resetBoard);
 });
