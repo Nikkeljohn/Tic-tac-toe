036d4157cb929ebe04d2a3be5963da0ad846d264
Nikkeljohn <nikkeljohn120@gmail.com>
2022-10-24 21:17:48 +0000

    changged size of reset button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 // added an event listener to get game button element and content
 
 window.addEventListener('DOMContentLoaded', () =>{
-    const announcer = document.querySelector('.announcer');
     const playerDisplay = document.querySelector('.display-player');
     const tile = Array.from(document.querySelectorAll('.tile'));
     const resetButton = document.querySelector('#reset');
