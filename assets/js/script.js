cd7307325c9ad34078b1e03a017032715886e892
Nikkeljohn <nikkeljohn120@gmail.com>
2022-10-25 09:58:07 +0000

    deleted all file and restart html file agine by creating new div and section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 // added an event listener to get game button element and content
 
-window.addEventListener('DOMContentLoaded', () =>{
-    const playerDisplay = document.querySelector('.display-player');
-    const tile = Array.from(document.querySelectorAll('.tile'));
-    const resetButton = document.querySelector('#reset');
+//window.addEventListener('DOMContentLoaded', () =>{
+    //const playerDisplay = document.querySelector('.display-player');
+    //const tile = Array.from(document.querySelectorAll('.tile'));
+   // const resetButton = document.querySelector('#reset');//
 
-    resetButton.addEventListener('click', resetBoard);
-});
+    //resetButton.addEventListener('click', resetBoard);//
+//});//
