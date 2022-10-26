c6a273bc9fe31b4060b0a624f8468a40d746fefb
Nikkeljohn <nikkeljohn120@gmail.com>
2022-10-26 19:37:22 +0000

    added two variable to track current player and a boolean to track if game is active or running

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,30 @@ window.addEventListener('DOMContentLoaded', () =>{
     const playerDisplay = document.querySelector('.display-player');
     const cell = Array.from(document.querySelectorAll('.tile'));
  const resetButton = document.querySelector('#reset');
- const announcer=document.querySelector(".announcer");
+let board =["","","","","","","","","",]
 
- //
+//track current player
 
-    resetButton.addEventListener('click', resetBoard);
+let currentPlayer="1";
+
+//boolean is used to track if game is running
+
+let gameRunning= false;
+
+ //giving winning condition
+ const winConditions =[
+    //here we declare 9 cells or tiles 0 to 9 for winning condition
+
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [2,5,8],
+    [1,4,7],
+    [0,3,6],
+    [0,4,8],
+    [2,4,6]
+
+ ];
+
+    
 });
