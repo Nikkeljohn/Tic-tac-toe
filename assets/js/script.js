be1de7d6cff417c8ea5bbf4210b616e9f1a231b7
Nikkeljohn <nikkeljohn120@gmail.com>
2022-10-29 10:48:25 +0000

    added new function , and add few instruction to implement but diidnt work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,24 @@
 // added an event listener to get game button element and content
 
-window.addEventListener('DOMContentLoaded', () =>{
-    const playerDisplay = document.querySelector('.display-player');
-    const tiles = Array.from(document.querySelectorAll('.tile'));
+//(window.addEventListener('DOMContentLoaded', () =>{
+    const statusText= document.querySelector('#statusText');
+    const tiles = document.querySelectorAll(".tile");
  const resetButton = document.querySelector('#reset');
 let board =["","","","","","","","","",]
 
 //track current player
 
-let currentPlayer="1";
+let currentPlayer = "X";
 
 //boolean is used to track if game is running
 
 let gameRunning= false;
 
  //giving winning condition
+  //here we declare 9 cells or tiles 0 to 9 for winning condition
+
  const winConditions =[
-    //here we declare 9 cells or tiles 0 to 9 for winning condition
+   
 
     [0,1,2],
     [3,4,5],
@@ -35,30 +37,48 @@ initializeGame();
 function initializeGame(){
     // used for each method and add event listner
 
-    tiles.forEach(tiles=>tiles.addEventListener("cilck","tilesClicked"));
-    resetButton.addEventListener("click","resetGame");
-    playerDisplay.textContent=`${currentPlayer}'s Turn`;
+    tiles.forEach(tile=>tile.addEventListener("cilck",tileClicked));
+    resetButton.addEventListener("click",resetGame);
+    statusText.textContent=`${currentPlayer}'s Turn`;
+    gameRunning = true;
 
 }
-function tilesClicked(){
+//when we click into our what will it do
+
+function tileClicked(){
 
     // this reffers to what ever tile we click on
 
     const tileIndex = this.getAttribute("tileIndex");
 
     // now have check if our tileIndexnumber within our board are not empty
-    if(board[tileIndex] !="" || !gameRunning){
+    if(board[tileIndex] != "" || !gameRunning){
         return;
     }
 
-}
-function updatetiles(tiles,index){
+    updateTile(this, tileIndex);
+    checkWinner();
+
 
 }
-function changePlayer(){
+
+function updatetiles(tile,index){
+    board[index] = currentPlayer;
+    tile.textContent = currentPlayer;
+
 
 }
+function changePlayer(){ 
+    currentPlayer = (currentPlayer == "X") ? "O":"X";
+    statusText.textContent = `${currentPlayer}'s Trun`
+
+
+
+}
+function checkWinner(){
+   
+}
+
 function resetGame(){
 
 }
-});
