7fcf814f3de7f157b6c7f6b7b660f6dc118e3539
Nikkeljohn <nikkeljohn120@gmail.com>
2022-10-30 09:18:12 +0000

    added functions to test the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,54 +38,22 @@ let gameRunning= true;
 
 // adding the  function for game 
 
-function handleValidation(){
-    let roundWon = false;
-    const winConditions = winConditions[i];
-    const a = options[winConditions[0]];
-    const b = options[winConditions[1]];
-    const c = options[winConditions[2]];
-    if (a === "" || b === "" || c === ""){
-        continue;
-    }
-    if (a ===b && b === c){
-
-    }
+function InitiateGmae(){
 
 }
-const userAction = (cell, index) =>{
-    if()
+function CilckTiles(index, cell){
 
+}
+function updateTile(){
 
 }
 
-const updateCells = (index) => {
-    options[index] = currentPlayer;
+function changePlayer(){
+
 
 }
-const changePlayer = () => {
-    playerDisplay.classList.remove(`player${currentPlayer}`);
-    currentPlayer = currentPlayer === "1" ? "2" : "1";
-    playerDisplay.innerText = currentPlayer;
-    playerDisplay.classList.add(`player${currentPlayer}`)
-
-}
-// restart the board//
-
-const resetBoard = () =>{
-    options = ["","","","","","","","",""];
-    gameRunning = true;
-    announcer.classList.add("hide");
-    if (currentPlayer === "2")
-    changePlayer();
-}
-
-tiles.forEach( cell => {
-    cell.innerText ="";
-    cell.classList.remove("player1");
-    cell.classList.remove("player2");
-});
-}
-// event lister for every tile by for each method//
+function restartGame()
+{// event lister for every tile by for each method//
 
     /*tiles.forEach(cell, index) => {
     cell.addEventListener("click", () => userAction(cell, index));
